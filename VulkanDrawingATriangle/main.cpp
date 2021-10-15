936a455af7c69591b5ae33bbcadf94d7bc7eeadc
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-10-15 00:55:07 -0300

    Implementation of the creation of vulkan instance method

--- VulkanDrawingATriangle/main.cpp
+++ VulkanDrawingATriangle/main.cpp
@@ -1,9 +1,12 @@
 #define GLFW_INCLUIDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vulkan/vulkan.h>
+
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -20,7 +23,9 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
+private:
 	void initWindow()
 	{
 		glfwInit();
@@ -33,6 +38,52 @@ private:
 
 	void initVulkan()
 	{
+		createInstance();
+	}
+
+	void createInstance()
+	{
+		VkApplicationInfo appInfo {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.ppEnabledLayerNames = 0;
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "available extensions\n";
+
+		for (const auto& extension : extensions)
+		{
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+
 
 	}
 
@@ -46,6 +97,8 @@ private:
 
 	void cleanup()
 	{
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
