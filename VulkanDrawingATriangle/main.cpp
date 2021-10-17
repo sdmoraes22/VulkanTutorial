fdb2b2e259f8a4cfb1e3d10ea0c97c53752d9906
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-10-17 00:50:06 -0300

    Enabling the validation layer

--- VulkanDrawingATriangle/main.cpp
+++ VulkanDrawingATriangle/main.cpp
@@ -7,9 +7,21 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <cstring>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validantionLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif // NDEBUG
+
 
 class HelloTriangleApplication {
 public:
@@ -26,6 +38,36 @@ private:
 	VkInstance instance;
 
 private:
+	bool checkValidationLayerSupport() 
+	{
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char* layerName : validantionLayers)
+		{
+			bool layerFound = false;
+
+			for (const auto& layerProperties : availableLayers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0)
+				{
+					layerFound = true;
+					break;
+				}
+			}
+
+			if (!layerFound)
+			{
+				return false;
+			}
+		}
+
+		return true;
+
+	}
 	void initWindow()
 	{
 		glfwInit();
@@ -33,7 +75,6 @@ private:
 		glfwInitHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-
 	}
 
 	void initVulkan()
@@ -43,6 +84,11 @@ private:
 
 	void createInstance()
 	{
+		
+		if (enableValidationLayers && !checkValidationLayerSupport()) {
+			throw std::runtime_error("validation layers requested, but not available!");
+		}
+
 		VkApplicationInfo appInfo {};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "Hello Triangle";
@@ -54,6 +100,16 @@ private:
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
+
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validantionLayers.size());
+			createInfo.ppEnabledLayerNames = validantionLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
 
 		uint32_t glfwExtensionCount = 0;
 		const char** glfwExtensions;
